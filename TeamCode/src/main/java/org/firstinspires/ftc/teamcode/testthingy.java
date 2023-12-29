90c9c1ce0987e8d00e86dda217c0b1ac071b24a8
nathan ricker <nathanricker44@gmail.com>
2023-12-28 20:10:13 -0600

    roadrunner nonsense again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testthingy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testthingy.java
@@ -4,10 +4,11 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.TankDrive;
-
+@Autonomous
 public final class testthingy extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
